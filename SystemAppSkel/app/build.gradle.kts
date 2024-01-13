3f80552107e3816c18ade3a7c004fb5b8e46de80
Yongwoo Park <yongwoo.park@42dot.ai>
2024-01-13 20:45:28 +0900

    add SystemAppSkel with builder mock

--- SystemAppSkel/app/build.gradle.kts
+++ SystemAppSkel/app/build.gradle.kts
@@ -54,8 +54,8 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx")
+    implementation("androidx.appcompat:appcompat")
+    implementation("androidx.core:core-ktx:1.9.0-alpha3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx")
     implementation("androidx.activity:activity-compose")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
@@ -63,6 +63,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
